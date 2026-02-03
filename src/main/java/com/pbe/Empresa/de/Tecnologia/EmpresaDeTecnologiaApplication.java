ca85af55e0bd43657833cf57b52d981382a179d7
JOÃO PEDRO CAVALCANTE SCRIVANI <joao.scrivani@senaisp.edu.br>
2026-02-03 07:58:46 -0300

    Empressa de informatica

---
+++ src/main/java/com/pbe/Empresa/de/Tecnologia/EmpresaDeTecnologiaApplication.java
@@ -0,0 +1,13 @@
+package com.pbe.Empresa.de.Tecnologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaDeTecnologiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaDeTecnologiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pbe/Empresa/de/Tecnologia/EmpresaDeTecnologiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbe.Empresa.de.Tecnologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresaDeTecnologiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
